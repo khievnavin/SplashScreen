92a15b3001232b34b7dd8ef228ba2f7f46bd5151
khiev navin <khievnavin@gmail.com>
2021-03-18 12:37:39 +0700

    Background

--- app/src/main/java/com/example/splashscreen/HomeActivity.java
+++ app/src/main/java/com/example/splashscreen/HomeActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -18,6 +19,7 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         name = findViewById(R.id.etxtname);
         Id = findViewById(R.id.etxtid);
